958246c8898b7e63c94682f065dcff58f5b5db0e
Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
2016-03-04 00:36:03 +0200

    intel_sgx: driver for Intel Software Guard Extensions
    Intel SGX is a set of CPU instructions that can be used by applications
    to set aside private regions of code and data.  The code outside the
    enclave is disallowed to access the memory inside the enclave by the CPU
    access control.
    
    SGX driver provides a ioctl API for loading and initializing enclaves.
    Address range for enclaves is reserved with mmap() and they are
    destroyed with munmap(). Enclave construction, measurement and
    initialization is done with the provided the ioctl API.
    
    The driver implements also a swapper thread ksgxswapd for EPC pages
    backed by a private shmem file. Currently it has a limitation of not
    swapping VA pages but there is nothing preventing to implement it later
    on. Now it was scoped out in order to keep the implementation simple.
    
    The parameter struct for SGX_IOC_ENCLAVE_INIT does not contain a
    parameter to supply a launch token. Generating and using tokens is best
    to be kept in the control of the kernel because it has direct binding to
    the IA32_SGXPUBKEYHASHx MSRs (a core must have MSRs set to the same
    value as the signer of token).
    
    By giving user space any role in the launch process is a risk for
    introducing bottlenecks as kernel must exhibit behavior that user space
    launch daemon depends on, properietary risks (closed launch daemons on
    closed platforms) and stability risks as there would be division of
    semantics between user space and kernel.
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Tested-by: Serge Ayoun <serge.ayoun@intel.com>

---
+++ drivers/platform/x86/intel_sgx/sgx_util.c
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
+// Copyright(c) 2016-17 Intel Corporation.
+//
+// Authors:
+//
+// Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
+// Suresh Siddha <suresh.b.siddha@intel.com>
+// Serge Ayoun <serge.ayoun@intel.com>
+// Shay Katz-zamir <shay.katz-zamir@intel.com>
+// Sean Christopherson <sean.j.christopherson@intel.com>
+
+#include <linux/shmem_fs.h>
+#include "sgx.h"
+
+struct page *sgx_get_backing(struct sgx_encl *encl,
+			     struct sgx_encl_page *entry,
+			     bool pcmd)
+{
+	struct address_space *mapping;
+	struct inode *inode;
+	gfp_t gfpmask;
+	pgoff_t index;
+
+	if (pcmd)
+		inode = encl->pcmd->f_path.dentry->d_inode;
+	else
+		inode = encl->backing->f_path.dentry->d_inode;
+
+	mapping = inode->i_mapping;
+	gfpmask = mapping_gfp_mask(mapping);
+
+	if (pcmd)
+		index = PFN_DOWN(entry->desc - encl->base) >> 5;
+	else
+		index = PFN_DOWN(entry->desc - encl->base);
+
+	return shmem_read_mapping_page_gfp(mapping, index, gfpmask);
+}
+
+void sgx_put_backing(struct page *backing_page, bool write)
+{
+	if (write)
+		set_page_dirty(backing_page);
+
+	put_page(backing_page);
+}
+
+static void sgx_zap_tcs_ptes(struct sgx_encl *encl)
+{
+	struct sgx_encl_page *entry;
+	struct radix_tree_iter iter;
+	struct vm_area_struct *vma;
+	unsigned long addr;
+	void **slot;
+
+	radix_tree_for_each_slot(slot, &encl->page_tree, &iter, 0) {
+		entry = *slot;
+		addr = SGX_ENCL_PAGE_ADDR(entry);
+
+		/* Skip regular and unloaded TCS pages. */
+		if (!entry->epc_page || !(entry->desc & SGX_ENCL_PAGE_TCS))
+			continue;
+
+		if (sgx_encl_find(encl->mm, addr, &vma))
+			continue;
+
+		zap_vma_ptes(vma, addr, PAGE_SIZE);
+	}
+}
+
+/**
+ * sgx_encl_invalidate - stop the enclave
+ * @encl:	an enclave
+ * @flush_cpus:	kick out the hardware threads.
+ *
+ * Removes all the TCS entries from an enclave and optionally flushes the
+ * hardware threads. On suspend, we do not flush hardware threads because
+ * nothing is running at that point.
+ */
+void sgx_invalidate(struct sgx_encl *encl, bool flush_cpus)
+{
+	sgx_zap_tcs_ptes(encl);
+
+	encl->flags |= SGX_ENCL_DEAD;
+
+	if (flush_cpus)
+		sgx_flush_cpus(encl);
+}
+
+static void sgx_ipi_cb(void *info)
+{
+}
+
+void sgx_flush_cpus(struct sgx_encl *encl)
+{
+	on_each_cpu_mask(mm_cpumask(encl->mm), sgx_ipi_cb, NULL, 1);
+}
